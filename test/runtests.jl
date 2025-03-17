29baa29e8eb0f9b647c4095368d72fea2a95c6cb
Savya Mishra <savyamishra21@gmail.com>
2025-03-17 18:04:51 +0530

    minor changes

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,7 @@
-using OptimizationSteadyStateGradient
+using OptimizationODE
 using Test
 
-using OptimizationSteadyStateGradient
-using Test
-using DifferentialEquations
-
-@testset "OptimizationSteadyStateGradient.jl" begin
-    # Test with Rosenbrock function
+@testset "OptimizationODE.jl" begin
     function rosenbrock(u, p)
         (p[1] - u[1])^2 + p[2] * (u[2] - u[1]^2)^2
     end
@@ -16,10 +11,8 @@ using DifferentialEquations
     optf = OptimizationFunction(rosenbrock, AutoForwardDiff())
     prob = OptimizationProblem(optf, u0, p)
     
-    # Solve with SteadyStateGradientOptimizer
-    sol = solve(prob, SteadyStateGradientOptimizer())
+    sol = solve(prob, ODESteadyStateOptimizer())
     
-    # Check that solution is close to the expected minimum [1.0, 1.0]
     @test isapprox(sol[1], 1.0, atol=1e-3)
     @test isapprox(sol[2], 1.0, atol=1e-3)
 end
