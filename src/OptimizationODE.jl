01c398af3c8c078300e9b278885aee8f3df4a6f3
Savya Mishra <savyamishra21@gmail.com>
2025-03-17 17:59:16 +0530

    changes to OptimizationODE.jl

--- src/OptimizationODE.jl
+++ src/OptimizationODE.jl
@@ -1,12 +1,51 @@
-module OptimizationSteadyStateGradient
+module OptimizationODE
 
 using Reexport
 @reexport using Optimization
-using LinearAlgebra
+using Optimization.SciMLBase
 
-include("steady_state_gradient.jl")
+export ODESteadyStateOptimizer
 
-export SteadyStateGradientOptimizer
+struct ODESteadyStateOptimizer <: Optimization.AbstractOptimizer
+    ss_solver
+    maxiters::Int  
+end
 
+ODESteadyStateOptimizer(; 
+    ss_solver = DynamicSS(Rodas5(autodiff=false)), 
+    maxiters = Int(1e6)
+) = ODESteadyStateOptimizer(ss_solver, maxiters)
 
+function SciMLBase.solve(prob::Optimization.OptimizationProblem, opt::ODESteadyStateOptimizer; 
+                         callback = nothing, kwargs...)
+    u0, p = prob.u0, prob.p
+    
+    opt_cache = Optimization.OptimizationCache(prob)
+    optf = opt_cache.f
+    
+    function gradient_flow!(du, u, p, t)
+        optf.grad(du, u, p)
+        du .= -du
+    end
+    
+    ss_prob = SteadyStateProblem(gradient_flow!, u0, p)
+    
+    sol = solve(ss_prob, opt.ss_solver; 
+                maxiters = opt.maxiters, 
+                kwargs...)
+    
+    u_final = sol.u
+    
+    obj_final = optf.f(u_final, p)
+    
+    return SciMLBase.OptimizationSolution(
+        u_final,            
+        opt_cache,            
+        opt,                
+        obj_final,  
+        sol.retcode,          
+        sol,
+        nothing                
+    )
+end
 end
